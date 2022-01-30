c2b0693cf6feb1d5f76cf989d98e375a20c723aa
yuto730 <my30.design@gmail.com>
2022-01-30 17:46:40 +0900

    db接続

---
+++ models/__init__.py
@@ -0,0 +1,5 @@
+from .models import Contact
+
+__all__ = [
+    Contact,
+]
--- models/contact.py
+++
@@ -1,5 +0,0 @@
-from database import db
-
-class Contact(db.Model):
-    __tablename__ = 'contacts'
-    id = db.Column(db.Integer, primary_key=True)
