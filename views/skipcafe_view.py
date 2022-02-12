506cf7860cff14f11e5d6a5e85ab3e02c72062a6
yuto730 <my30.design@gmail.com>
2022-02-12 09:45:29 +0900

    “問い合わせフォームの作成”

--- views/skipcafe_view.py
+++ views/skipcafe_view.py
@@ -1,5 +1,6 @@
-from flask import Blueprint, render_template
-# from models.contact import Contact
+from flask import Blueprint, render_template, request, redirect, url_for
+from database import db
+from models import Contact
 
 # Blueprintのオブジェクトを生成する
 app = Blueprint('views', __name__)
@@ -30,9 +31,28 @@ def access():
     caption = 'お店案内'
     return render_template('access.html', title=title, subtitle=subtitle, caption=caption)
 
-@app.route('/contact')
+@app.route('/contact', methods=['GET', 'POST'])
 def contact():
-    title = "Skipcafe|Contact"
-    subtitle = 'Contact'
-    caption = 'お問い合わせ'
-    return render_template('contact.html', title=title, subtitle=subtitle, caption=caption)
+    if request.method == 'GET':
+        title = "Skipcafe|Contact"
+        subtitle = 'Contact'
+        caption = 'お問い合わせ'
+        return render_template('contact.html', title=title, subtitle=subtitle, caption=caption)
+
+    if request.method == 'POST':
+        form_name = request.form.get('name') # str
+        form_url = request.form.get('url') # str
+        form_mail = request.form.get('mail') # str
+        form_mail_confirmation = request.form.get('mail_confirmation') # str
+        form_message = request.form.get('message') # str
+
+        contact = Contact(
+            name=form_name,
+            url=form_url,
+            mail=form_mail,
+            mail_confirmation=form_mail_confirmation,
+            message=form_message
+        )
+        db.session.add(contact)
+        db.session.commit()
+        return redirect(url_for('views.index'))
